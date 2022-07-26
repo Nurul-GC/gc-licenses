d49d60a3bb8a6a4d596bd9fe04ed7a59460cd70f
GitHub Action <action@github.com>
2022-07-26 08:24:27 +0000

    Template cleanup

--- src/main/kotlin/com/github/nurulgc/gclicenses/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/nurulgc/gclicenses/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.nurulgc.gclicenses.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.nurulgc.gclicenses.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/nurulgc/gclicenses/services/MyProjectService.kt
+++ src/main/kotlin/com/github/nurulgc/gclicenses/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.nurulgc.gclicenses.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.nurulgc.gclicenses.MyBundle
 
 class MyProjectService(project: Project) {
 
